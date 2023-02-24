0b273a4e91a8e1113e3a00fbf31fbdf0dc3d4d87
Putra B <cristminix@gmail.com>
2023-02-24 10:54:28 +0700

    add codeigniter 3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'damar/colab-admin-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b76ab5f7e22a442c6a1b68ee8e16de03e8a5d5a3',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'codeigniter/framework' => array(
+            'pretty_version' => '3.1.13',
+            'version' => '3.1.13.0',
+            'reference' => 'bcb17eb8ba53a85de154439d0ab8ff1bed047bc9',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../codeigniter/framework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'damar/colab-admin-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b76ab5f7e22a442c6a1b68ee8e16de03e8a5d5a3',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
