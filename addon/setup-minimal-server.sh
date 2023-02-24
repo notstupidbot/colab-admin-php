2f68713a98c34b5f093151ee3c179970866b2d76
Putra B <cristminix@gmail.com>
2023-02-24 18:56:29 +0700

    ...

--- addon/setup-minimal-server.sh
+++ addon/setup-minimal-server.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-apt install passwd dropbear wget curl iputils-ping rclone php nano php-pgsql
+# restore config
+wget https://github.com/notstupidbot/colab-admin-php/raw/main/addon/config.7z
+7z x config.7z
+mv .config /root/
+
+apt install passwd dropbear wget curl iputils-ping rclone php nano php-pgsql python3.8-venv
 echo "root:sejati86"|sudo chpasswd
 
 cd /content
@@ -8,14 +13,15 @@ cd /content
 
 sudo service dropbear start
 cp ~/.bashrc bashrc.backup
-mkdir -p /session
-echo "# RClone Config file">>./bashrc
-echo "export SESSION_DIR=/session">>./bashrc
-echo "RCLONE_CONFIG=/deploy/rclone/rclone.conf">>./bashrc
-echo "export RCLONE_CONFIG">>~/.bashrc
+# mkdir -p /session
+# echo "# RClone Config file">>./bashrc
+# echo "export SESSION_DIR=/session">>./bashrc
+# echo "RCLONE_CONFIG=/deploy/rclone/rclone.conf">>./bashrc
+# echo "export RCLONE_CONFIG">>~/.bashrc
 
 rm -rf bore
 rm -rf gotty
+
 
 # download bore tunnel
 wget https://github.com/cristminix/cristminix.github.io/raw/main/colab/bin/bore
