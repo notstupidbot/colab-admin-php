eb6ed0a34906da28762e85bb5809441816b916ad
Putra B <cristminix@gmail.com>
2023-02-26 11:19:23 +0700

    ...

--- addon/tts.sh
+++ addon/tts.sh
@@ -3,7 +3,7 @@
 cd /content/tts-venv
 source bin/activate
 tts --text "$1" \
-    --model_path checkpoint.pth \
-    --config_path config.json \
+    --model_path "$3/checkpoint.pth" \
+    --config_path "$3/config.json" \
     --speaker_idx wibowo \
     --out_path "$2"
