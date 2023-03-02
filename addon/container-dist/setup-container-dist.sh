152bebe5a624f91073705339c5d7552d775a5973
Putra B <cristminix@gmail.com>
2023-03-02 20:43:12 +0700

    ...

--- addon/container-dist/setup-container-dist.sh
+++ addon/container-dist/setup-container-dist.sh
@@ -1,6 +1,5 @@
 cd /content
 
-
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1TmT0a3wPyrXrNAjujtPzkCRUeSxoFybG' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1TmT0a3wPyrXrNAjujtPzkCRUeSxoFybG" -O dist.7z && rm -rf /tmp/cookies.txt
 
 mkdir -p /container/dist
@@ -20,23 +19,10 @@ wget https://github.com/notstupidbot/colab-admin-php/raw/main/addon/config.7z
 7z x config.7z
 mv /root/.config /root/.config.old
 cp -r .config /root/
-
-# mount gdrive
-mkdir -p /content/gdrive
-rclone mount gdrive: /content/gdrive --daemon
-
-#starting dropbear
-/container/dist/etc/init.d/dropbear start
-
+ 
 #starting vsftpd
 cp /container/dist/etc/vsftpd.conf /etc/
 cp /container/dist/etc/ftpusers /etc/
-
-/container/dist/etc/init.d/vsftpd start
-
-# starting gotty
-
-/container/dist/etc/init.d/gotty start
 
 # apt install passwd
 echo "Update root password"
@@ -54,7 +40,21 @@ useradd -m -p sejati86 postgres
 cp -r /container/dist/etc/postgresql/10/pgsql /container/dist/etc/postgresql/10/main
 chown -R postgres:postgres /container/dist/etc/postgresql/10/main
 
-echo "Starting pgsql"
-/container/etc/init.d/pgsql start
 
+# mount gdrive
+echo "Mounting gdrive"
+mkdir -p /content/gdrive
+rclone mount gdrive: /content/gdrive --daemon
+#echo "Starting pgsql"
+/container/etc/init.d/pgsql start
+#start dev-server
+/container/etc/init.d/dev-server start
+#start push server
+/container/etc/init.d/push-server start
+# starting gotty
+/container/dist/etc/init.d/gotty start
+#starting ftp
+/container/dist/etc/init.d/vsftpd start
+#starting dropbear
+/container/dist/etc/init.d/dropbear start
 #./pg_ctl -D /container/dist/pgsql_data/10/main -l logfile start
