c4924b8643fde293983888cdabb27959b2df5468
Putra B <cristminix@gmail.com>
2023-03-02 21:06:35 +0700

    ..

--- addon/container-dist/setup-container-dist.sh
+++ addon/container-dist/setup-container-dist.sh
@@ -1,6 +1,6 @@
 cd /content
 file_id="15LUqVtuEXBo6h3e0Gm_upAqslQ-BMQtS"
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$file_id' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=file_id" -O dist.7z && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=15LUqVtuEXBo6h3e0Gm_upAqslQ-BMQtS' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=15LUqVtuEXBo6h3e0Gm_upAqslQ-BMQtS" -O dist.7z && rm -rf /tmp/cookies.txt
 
 mkdir -p /container/dist
 mkdir -p /container/src
