c63c4ef9b5e4b39f5a6757506293c1590416c804
Putra B <cristminix@gmail.com>
2023-03-08 20:59:55 +0700

    ,,

--- addon/push-server.php
+++ addon/push-server.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__DIR__) . './vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 // require_once dirname(__DIR__) . './addon/Pusher.php';
 
 $loop 	= React\EventLoop\Factory::create();
