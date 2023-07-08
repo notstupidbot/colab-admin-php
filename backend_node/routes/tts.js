287ae0a9e6278ca0368d97b6bab91e7c92e6b311
cristminix <cristminix@gmail.com>
2023-07-09 01:09:17 +0700

    fix convertTtf server

--- backend_node/routes/tts.js
+++ backend_node/routes/tts.js
@@ -1,5 +1,8 @@
 
 import express from "express"
+import fetch from 'node-fetch'
+import {SocksProxyAgent} from 'socks-proxy-agent'
+
 import {calculateOffset, calculateTotalPages, toSqliteDateTime} from "./fn.js"
 
 // Retrieve all users
@@ -10,10 +13,212 @@ import {
 import multer from 'multer'
 import TtsProject from "../models/TtsProject.js"
 import TtsSentence from "../models/TtsSentence.js"
-
 import Preference from "../models/Preference.js"
+import WordList from "../models/WordList.js"
+import WordListTtf from "../models/WordListTtf.js"
 // Configure Multer for handling multipart/form-data
 const upload = multer()
+
+const getTtsPrefs = async(manager)=>{
+    let ttsServerEndPoint, ttsServerProxy, ttsServerUseProxy
+
+    const group = 'tts_server' 
+    const prefs = await manager.find(Preference, {group})
+
+    if(prefs){
+        if(prefs.length){
+            for(let i in prefs){
+                const pref = prefs[i]
+                try{
+                    if(pref.key == 'tts_server.endpoint'){
+                        ttsServerEndPoint = JSON.parse(pref.val).url
+                    }
+                    if(pref.key == 'tts_server.proxy'){
+                        ttsServerProxy = JSON.parse(pref.val).url
+                    }
+                    if(pref.key == 'tts_server.enable_proxy'){
+                        ttsServerUseProxy = JSON.parse(pref.val).value
+                    }
+                }catch(e){
+                    console.error(e)
+                }
+            }
+        }
+    }
+
+    return [ttsServerEndPoint, ttsServerProxy, ttsServerUseProxy]
+}
+
+class MWordList {
+    ds = null
+    mWordListTtf = null
+
+    constructor(ds){
+        this.ds = ds
+    }
+
+    async create(content){
+        const {manager} = this.ds
+
+        content = content.toLowerCase() 
+
+        const existing = await this.getByWord(content)
+
+        if(existing){
+            return existing
+        }
+        const wordList = new WordList()
+        wordList.content = content
+        wordList.create_date = toSqliteDateTime(new Date)
+        wordList.last_updated = toSqliteDateTime(new Date)
+        
+        const record = await manager.save(wordList)
+        return record
+
+    }
+    async getByWord(content){
+        content = content.toLowerCase()
+        const {manager} = this.ds
+        const record = await manager.findOne(WordList, {where:{content}})
+
+        return record
+    }
+    setMWordListTtf(mWordListTtf){
+        this.mWordListTtf = mWordListTtf
+    }
+}
+class MWordListTtf {
+    ds = null
+    mWordList = null
+    convertPrefs = null
+    constructor(ds, mWordList){
+        this.ds = ds
+        this.mWordList = mWordList
+    }
+    async getByWord(text){
+        text = text.toLowerCase()
+        const word = await this.mWordList.create(text)
+        // console.log(word)
+        const record = await this.getByWordId(word.id)
+
+        return record
+    }
+    async getByWordId(word_id){
+        const {manager} = this.ds
+        const record = await manager.findOne(WordListTtf, {where:{word_id}})
+        
+        return record
+    }
+    async create(text, content){
+        const {manager} = this.ds
+
+        text = text.toLowerCase()
+
+        let existingRecByContentCheck = await this.getByContent(content)
+        if(existingRecByContentCheck){
+            return existingRecByContentCheck
+        }
+
+        let word = await this.mWordList.getByWord(text)
+        const wordListTtf = new WordListTtf()
+        wordListTtf.content = content
+        wordListTtf.path = ""
+        wordListTtf.word_id = word.id
+        wordListTtf.create_date = toSqliteDateTime(new Date)
+        wordListTtf.last_updated = toSqliteDateTime(new Date)
+        const record = await manager.save(wordListTtf)
+        return record
+
+    }
+    async convertTtfServer(text){
+        let ttsServerEndPoint, ttsServerProxy, ttsServerUseProxy 
+        const {manager} = this.ds
+
+        if(!this.convertPrefs){
+            
+            this.convertPrefs = await getTtsPrefs(manager)
+            
+        }
+        [ttsServerEndPoint, ttsServerProxy, ttsServerUseProxy] = this.convertPrefs
+        
+        let requestOptions = {}
+
+        if(ttsServerUseProxy){
+            requestOptions.agent = new SocksProxyAgent(ttsServerProxy) 
+        }
+        const url = `${ttsServerEndPoint}/api/convert?text=${encodeURI(text)}`
+        console.log(url)
+        const result = await fetch(url, requestOptions)
+                            .then(response => response.text())
+                            .then(data => {
+                                return data
+                            })
+                            .catch(error => {
+                                console.error('Error:', error)
+
+                                return ''
+                            })
+        console.log(result)
+        
+        return result
+
+    }
+    async getByContent(content){
+        const {manager} = this.ds
+        const record = await manager.findOne(WordListTtf, {where:{content}})
+
+        return record
+    }
+    async convert(text){
+        text = text.toLowerCase()
+        let wordTtf = await this.getByWord(text)
+
+        let result = ''
+        if(!wordTtf){
+            let content = await this.convertTtfServer(text)
+            if(content){
+                if(content.length > 0){
+                    wordTtf = await this.create(text, content.trim())
+                    result = wordTtf.content
+                }
+            }else{
+                console.log(`convertTtfServer return blank words`)
+            }
+        }else{
+            result = wordTtf.content
+        }
+
+        return result
+    }
+
+}
+  
+
+
+
+router.get('/convert', async (req, res) => {
+    const {text} = req.query
+    const textSplit = text.split(' ')
+    let outputText = []
+    
+    const mWordList = new MWordList(AppDataSource)
+    const mWordListTtf = new MWordListTtf(AppDataSource, mWordList)
+    mWordList.setMWordListTtf(mWordListTtf)
+
+    for(let i in textSplit){
+        const word = textSplit[i].replace(/[^a-z]/,'')
+        if(word.length > 0){
+            const ttf = await mWordListTtf.convert(word)
+            const ttfItem = {
+                text : word,
+                ttf 
+            }
+            outputText.push(ttfItem)
+        }
+    }
+    res.send(outputText)
+
+})
 
 router.post('/sentence',upload.any(), async (req, res) => {
     // Route logic for handling POST '/tts-sentence/create'
